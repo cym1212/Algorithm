63c6230d5a3451beff3de8b2e49d82b5df6864c9
cym1212 <109451404+cym1212@users.noreply.github.com>
2024-09-05 15:09:48 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.13 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        int sum = a + b + c;
+        int sum2 = ((int)Math.pow(a,2) + (int)Math.pow(b,2) + (int)Math.pow(c,2));
+        int sum3 = ((int)Math.pow(a,3) + (int)Math.pow(b,3) + (int)Math.pow(c,3));
+        
+        if (a == b && b == c){
+            answer = sum * sum2 * sum3;
+        } else if(a ==b || b==c || c==a ){
+            answer = sum * sum2;
+        } else{
+            answer = sum;
+        }
+        return answer;
+    }
+}
