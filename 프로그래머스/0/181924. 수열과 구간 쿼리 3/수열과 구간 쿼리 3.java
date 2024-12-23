1e66c165444d24ba183e576607eb893f65923c8c
cym1212 <109451404+cym1212@users.noreply.github.com>
2024-12-23 12:55:09 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+
+        int a = 0;
+        for(int i=0; i< queries.length; i++){
+            a = arr[queries[i][0]];
+            arr[queries[i][0]] = arr[queries[i][1]];
+            arr[queries[i][1]] = a;
+                                    
+        }
+        return arr;
+                
+                
+    }
+
+}
